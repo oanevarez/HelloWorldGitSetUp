48799c8afa22b8e251b769e9e754bbd4a22976d7
osvaldonevarez <ozzynevar.oa@outlook.com>
2024-03-04 20:49:00 -0700

    feat(TestGitHub): Added Boiler Plate Code

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String [] args){
+        /*
+        Print To Console
+         */
+        String prompt = "Hello World";
+        System.out.println(prompt.toUpperCase());
+    }
+}
